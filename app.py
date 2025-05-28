62cea5f499522eafc85ef1627aa05bdc39bf3977
Ionut-hypershine <ionutciulei45@yahoo.com>
2025-05-28 18:43:07 +0100

    Repar funcția jobs

--- app.py
+++ app.py
@@ -58,7 +58,7 @@ def add_payslip():
             conn.commit()
         return redirect(url_for("admin"))
     return render_template("add_payslip.html")
-@@app.route("/jobs")
+@app.route("/jobs")
 def jobs():
     conn = sqlite3.connect("vms.db")
     c = conn.cursor()
